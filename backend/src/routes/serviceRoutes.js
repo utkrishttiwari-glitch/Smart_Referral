6693349dbbc3618bc90f16868b8d8bd9d534049d
utkrishttiwari-glitch <utkrishttiwari67@gmail.com>
2026-09-11 17:51:10 +0530

    ds

--- backend/src/routes/serviceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getServices } from "../controllers/serviceController.js";
-
-const router = express.Router();
-
-router.get("/", getServices);
-
-export default router;
--- frontend/src/Pages/DoctorInstantReferral.jsx
+++
@@ -1,7 +0,0 @@
-import DoctorReferral from "./DoctorReferral";
-
-function DoctorInstantReferral() {
-  return <DoctorReferral instantMode />;
-}
-
-export default DoctorInstantReferral;
